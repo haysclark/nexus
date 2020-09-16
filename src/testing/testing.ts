9cb388311d2a0a1d7aae2f916fd61be25dd612ed
Hays Clark <hays.clark@gmail.com>
2020-09-15 17:59:58 -0700

    fix: get around port collisions with a random port
    - port workaround allows more performant parallel testing
    - testing on CI environments should still utilize Jest’s `—runInBand` flag
    - resolves #1326

--- src/testing/testing.ts
+++ src/testing/testing.ts
@@ -78,7 +78,8 @@ export async function createTestContext(opts?: CreateTestContextOptions): Promis
   // todo figure out some caching system here, e.g. imagine jest --watch mode
   const layout = rightOrFatal(await Layout.create({ entrypointPath: opts?.entrypointPath, projectRoot: opts?.projectRoot }))
   const pluginManifests = await PluginWorktime.getUsedPlugins(layout)
-  const randomPort = await getPort({ port: getPort.makeRange(4000, 6000) })
+  const randomStartPort = Math.floor(Math.random() * (6000 - 4000)) + 4000
+  const randomPort = await getPort({ port: getPort.makeRange(randomStartPort, 6000) })
   const privateApp = app as PrivateApp
 
   const forcedServerSettings = {
